438a5f65da6b291c000293f007ea69897883f5b5
jimmaomao <493955352@qq.com>
2017-04-25 17:52:22 +0800

    新增USER接口
    新增USER接口

---
+++ MyLive.IDAL/InterfaceUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyLive.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLive.IDAL
+{
+    /// <summary>
+    /// 用户接口
+    /// </summary>
+    public interface InterfaceUserRepository : InterfaceBaseRepository<T_User>
+    {
+    }
+}
